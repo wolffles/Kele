38e9ecde0177962289bd6e2cc78616f2f0bbd259
wolffles <tofukillzstevent@live.com>
2017-09-18 16:10:33 -0700

    added authentication token to be used

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# Bapig
-
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/bapig`. To experiment with that code, run `bin/console` for an interactive prompt.
-
-TODO: Delete this and the text above, and describe your gem
+# BAPIG
+#### stands for Bloc API Gem
 
 ## Installation
 
@@ -37,7 +34,3 @@ Bug reports and pull requests are welcome on GitHub at https://github.com/[USERN
 ## License
 
 The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
-
-## Code of Conduct
-
-Everyone interacting in the Bapig project’s codebases, issue trackers, chat rooms and mailing lists is expected to follow the [code of conduct](https://github.com/[USERNAME]/bapig/blob/master/CODE_OF_CONDUCT.md).
--- lib/bapig.rb
+++ lib/bapig.rb
@@ -1,13 +1,26 @@
-class Kele
-  attr_accessor :username, :password
-  #just now realized what this does so im adding a note lol, attribute_accessor allows the class object to read or write to attributes that you set.
-  attr_reader :arbitrary
-  # this will not allow you to set a new value to it.
+require 'httparty'
+class Bapig
+  include HTTParty
 
-  def initialize(username, password, arbitrary)
-    @username = username
-    @password = password
-    @arbitrary = arbitrary
+
+  def initialize(email, password)
+    # httparty documentation
+    response = self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
+    #self.class.post is the same as <class_name>.post
+    puts response.code
+    raise "invalid email/password " if response.code == 401
+    @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+  end
+
+
+private
+
+  def base_api_endpoint(end_point)
+    "https://www.bloc.io/api/v1/#{end_point}"
   end
 
 end
